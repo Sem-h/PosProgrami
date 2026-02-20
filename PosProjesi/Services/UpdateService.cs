e4fc1e61c32b8b7e3a290b303b6d6c01059d44df
Sem-h <semih@mynet.com>
2026-02-20 21:45:39 +0300

    v1.0.2 - CurrentVersion güncellendi

--- PosProjesi/Services/UpdateService.cs
+++ PosProjesi/Services/UpdateService.cs
@@ -13,7 +13,7 @@ namespace PosProjesi.Services
 
     public class UpdateService : IDisposable
     {
-        public const string CurrentVersion = "1.0.0";
+        public const string CurrentVersion = "1.0.2";
 
         private const string BaseUrl =
             "https://raw.githubusercontent.com/Sem-h/PosProgrami/main";
